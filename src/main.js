6facb6ec94ae02a2a6a3a3b393564d82f7cffc36
nightblade9 <nightbladecodes@gmail.com>
2019-08-24 19:31:53 -0400

    Generate and display cover (purple)

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ Game = {
       Game.view.height = config("mapHeight") * config("tileSize") + config("footerHeight");
 
       Crafty.init(Game.view.width, Game.view.height);
-      this.grid = Crafty.e("Map")
+      this.grid = Crafty.e("Map"); // generates cover, too.
       grid.initialize(config("mapWidth"), config("mapHeight"));
       var tileSize = config("tileSize");
 
@@ -26,13 +26,14 @@ Game = {
         for (const x of Array(grid.width).keys()) {
           var gridTile = Crafty.e("GridTile").initialize(x, y).move(x * tileSize, y * tileSize);
           Game.gridTiles[x + ", " + y] = gridTile;
+          Game.applyColor(gridTile, x, y);
         }
       }
 
-      var reds = Game.randomlyCreateSquad(config("squadSize"), "red", 0, parseInt(config("mapWidth") / 3));
+      var reds = Game.randomlyCreateSquad(grid, config("squadSize"), "red", 0, parseInt(config("mapWidth") / 3));
       reds.forEach(red => Game.reds.push(red));
 
-      var blues = Game.randomlyCreateSquad(config("squadSize"), "blue", parseInt(config("mapWidth") * 2 / 3), config("mapWidth"));
+      var blues = Game.randomlyCreateSquad(grid, config("squadSize"), "blue", parseInt(config("mapWidth") * 2 / 3), config("mapWidth"));
       blues.forEach(blue => Game.blues.push(blue));
 
       Crafty.e("ControlPanel").move2(0, Game.view.height - config("footerHeight"));
@@ -45,7 +46,7 @@ Game = {
       for (const y of Array(grid.height).keys()) {
         for (const x of Array(grid.width).keys()) {
           var gridTile = Game.gridTiles[x + ", " + y];
-          if (Math.sqrt(Math.pow(x - tileX, 2) + Math.pow(y - tileY, 2)) <= Game.selected.moveDistance())
+          if (grid.isEmpty(x, y) && Math.sqrt(Math.pow(x - tileX, 2) + Math.pow(y - tileY, 2)) <= Game.selected.moveDistance())
           {
             gridTile.color("#00aa00");
           }
@@ -58,7 +59,7 @@ Game = {
       for (const y of Array(grid.height).keys()) {
         for (const x of Array(grid.width).keys()) {
           var gridTile = Game.gridTiles[x + ", " + y];
-          gridTile.color("#cccccc");
+          Game.applyColor(gridTile, x, y);
         }
       }
 
@@ -66,15 +67,24 @@ Game = {
       collection.forEach(blue => blue.css({ "border": "none"}));
     },
 
-    randomlyCreateSquad: function(size, team, minX, maxX) {
+    applyColor(gridTile, x, y) {
+      if (grid.get(x, y) === "lowCover") {
+        gridTile.color("#440088");
+      } else if (grid.get(x, y) === "highCover") {
+        gridTile.color("#8800ff");
+      } else {
+        gridTile.color("#cccccc");
+      }
+    },
+
+    randomlyCreateSquad: function(grid, size, team, minX, maxX) {
       var toReturn = [];
       var toCreate = size;
 
       while (toCreate > 0) {
         var x = randomBetween(minX, maxX);
         var y = randomBetween(0, config("mapHeight"));
-        var contents = grid.get(x, y);
-        if (typeof(contents) === "undefined" || contents === null) {
+        if (grid.isEmpty(x, y)) {
           var member = Crafty.e("SquadMate");
           member.initialize(team, grid, x, y);
           toCreate -= 1;
--- src/map.js
+++ src/map.js
@@ -3,6 +3,27 @@ Crafty.c("Map", {
         this.width = width;
         this.height = height;
         this.data = Array(width * height);
+
+        var coverLeft = config("numLowCover");
+        while (coverLeft > 0) {
+            var x = randomBetween(0, config("mapWidth"));
+            var y = randomBetween(0, config("mapHeight"));
+            if (grid.isEmpty(x, y)) {
+                grid.set(x, y, "lowCover");
+                coverLeft -= 1;
+            }
+        }
+
+        coverLeft = config("numHighCover");
+        while (coverLeft > 0) {
+            var x = randomBetween(0, config("mapWidth"));
+            var y = randomBetween(0, config("mapHeight"));
+            if (grid.isEmpty(x, y)) {
+                grid.set(x, y, "highCover");
+                coverLeft -= 1;
+            }
+        }
+        
     },
 
     set(x, y, contents) {
@@ -15,6 +36,11 @@ Crafty.c("Map", {
 
     get(x, y) {
         return this.data[this._index(x, y)];
+    },
+
+    isEmpty(x, y) {
+        var contents = this.get(x, y);
+        return typeof(contents) === "undefined" || contents === null;
     },
 
     _index(x, y) {
